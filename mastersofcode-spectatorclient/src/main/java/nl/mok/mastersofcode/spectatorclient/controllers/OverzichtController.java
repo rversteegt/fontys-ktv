a6a858581a7c1fb291df37cac111962bd1113d0c
gijshendrikx <gijshendrikx1990@gmail.com>
2015-11-07 18:04:21 +0100

    Added spring, pebble and a bootstrap theme with pages to the spectator-client

---
+++ mastersofcode-spectatorclient/src/main/java/nl/mok/mastersofcode/spectatorclient/controllers/OverzichtController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.mok.mastersofcode.spectatorclient.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Gijs
+ */
+@Controller
+@RequestMapping("/overzicht")
+public class OverzichtController {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView testDashboard(final HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView();
+        
+        mav.addObject("page", new Object() {
+            public String uri = "/mok/overzicht";
+            public String redirect = request.getRequestURL().toString();
+        });
+
+        mav.setViewName("index.twig");
+        
+        return mav;
+    }
+
+}
---
+++ mastersofcode-spectatorclient/src/main/java/nl/mok/mastersofcode/spectatorclient/controllers/RoundController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.mok.mastersofcode.spectatorclient.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Gijs
+ */
+@Controller
+@RequestMapping("/round")
+public class RoundController {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView testDashboard(final HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView();
+        
+        mav.addObject("page", new Object() {
+            public String uri = "/mok/round";
+            public String redirect = request.getRequestURL().toString();
+        });
+
+        mav.addObject("id", request.getParameter("id"));
+        mav.setViewName("round.twig");
+        
+        return mav;
+    }
+
+}
