e665647003403607879c5d8f8fc4eace79917b85
gijshendrikx <gijshendrikx1990@gmail.com>
2015-12-02 12:18:39 +0100

    Added data binding and Node Entity

---
+++ mastersofcode-adminclient/src/main/java/remote/ws/mok/domain/Node.java
@@ -0,0 +1,32 @@
+package remote.ws.mok.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ *
+ * @author Gijs
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Node {
+
+	private int id;
+
+	private String jndi;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getJndi() {
+		return jndi;
+	}
+
+	public void setJndi(String jndi) {
+		this.jndi = jndi;
+	}
+    
+}
